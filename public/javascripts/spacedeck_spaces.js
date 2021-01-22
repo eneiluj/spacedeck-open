76ab2da83f936499c3186435ffc878557198b341
Julien Veyssier <eneiluj@posteo.net>
2021-01-22 12:52:48 +0100

    avoid async loop launched twice, code cleanup
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- public/javascripts/spacedeck_spaces.js
+++ public/javascripts/spacedeck_spaces.js
@@ -224,14 +224,6 @@ var SpacedeckSpaces = {
 
             this.setup_watches();
 
-            /*
-            setInterval(function() {
-              console.debug('LAUNCH REFRESH')
-              this.refresh_space_artifacts(space._id);
-            }.bind(this), 5000);
-            */
-           this.start_loop(space)
-
             load_artifacts(space._id, function(artifacts) {
 
               // FIXME: how to cleanly handle this error?
@@ -332,63 +324,10 @@ var SpacedeckSpaces = {
         this.guest_nickname = "";
         userReady();
       }
-    },
 
-    refresh_space_artifacts: function(space_id) {
-      console.debug('refresh_space_artifacts OF ' + space_id)
-      console.debug(this)
-            load_artifacts(space_id, function(artifacts) {
+      // start async refresh loop
+      this.start_loop(space_id)
 
-              // FIXME: how to cleanly handle this error?
-              if (!artifacts) {
-                artifacts = [];
-              }
-              
-              // FIXME: remove kludge
-              console.debug(this)
-              for (var i=0; i<artifacts.length; i++) {
-                this.update_board_artifact_viewmodel(artifacts[i]);
-              }
-              this.active_space_artifacts = artifacts;
-
-              /*
-              this.$set("active_space", space);
-              this.active_space = space;
-
-              this.auth_websocket(this.active_space);
-
-              this.active_view = "space";
-              this.fixup_space_size();
-
-              if (space._id != this.active_space._id) {
-                this.present_mode = true;
-                this.active_space_is_readonly = true;
-              } else {
-                this.active_space_is_readonly = false;
-              }
-
-              this.discover_zones();
-
-              window.setTimeout(function() {
-                this.zoom_to_fit();
-              }.bind(this),10);
-
-              if (on_success) {
-                on_success();
-              }
-
-              this.active_space_loaded = true;
-              this.extract_properties_from_selection(); // populates zones etc
-              
-              load_comments(space._id, function(messages) {
-                if (!messages) messages = [];
-                
-                this.active_space_messages = messages;
-                this.refresh_space_comments();
-              }.bind(this), function(xhr) { console.error(xhr); });
-              */
-
-            }.bind(this));
     },
 
     refresh_space_comments: function() {
